accc11de3d8c1d9a555e7653a5991a315755c90b
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-12-03 16:07:29 +0200

    thunk type was added

--- src/store/thunks/index.ts
+++ src/store/thunks/index.ts
@@ -1,7 +1,14 @@
-import { AppDispatch } from "..";
+import { AnyAction } from "redux";
+import { ThunkAction } from "redux-thunk";
+import { AppDispatch, RootState } from "..";
 import { setTodo } from './../reducers/todo.reducer'
 
-export const getTodoThunk = (id: number = 1) => async (dispatch: AppDispatch) => {
+export const getTodoThunk = (id: number = 1): ThunkAction<
+    void,
+    RootState,
+    unknown,
+    AnyAction
+> => async (dispatch: AppDispatch) => {
     try {
         const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
         const data = await res.json();
